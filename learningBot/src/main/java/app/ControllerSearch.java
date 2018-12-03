655a688d83a5f63b5296e452ff32e26797a18bd7
Gabriel <gabrieltlopessjc@hotmail.com>
2018-12-03 09:36:42 -0200

    Class Diagram

--- learningBot/src/main/java/app/ControllerSearch.java
+++ learningBot/src/main/java/app/ControllerSearch.java
@@ -4,5 +4,7 @@ import com.pengrad.telegrambot.model.Update;
 public interface ControllerSearch {
 	
 	public void search(Update update);
+	
+	public void searchAPI(String latlon, String categoria) throws IOException;
 
 }
